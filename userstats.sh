99d4c189b398baed8815f8cb407bf1906951ed11
Isadora <flisadora@gmail.com>
2019-11-16 22:31:21 +0000

    group filter implementation to userstats.sh

--- userstats.sh
+++ userstats.sh
@@ -2,15 +2,20 @@
 
 # Função para contar o numero de utilizadores
 function countUsers(){
-    if [ $soId -eq 0 ]; then
-        users=($( last | awk '{print $1'} | sed 'N;$!P;$!D;$d' | sort -u ))
+    if [ ! -z $group ]; then
+        groupUsers=($( grep "$group:" /etc/group | cut -d ":" -f 4 ))
+        IFS=',' read -r -a users <<< "$groupUsers"
     else
-        users=($( last | awk '{print $1'} | head -n -2 | sort -u ))
+        if [ $soId -eq 0 ]; then
+            users=($( last | awk '{print $1'} | sed 'N;$!P;$!D;$d' | sort -u ))
+        else
+            users=($( last | awk '{print $1'} | head -n -2 | sort -u ))
+        fi
+        users=(${users[@]/"shutdown"})
+        users=(${users[@]/"reboot"})
+        users=(${users[@]/"root"})
+        users=(${users[@]/"_mbsetupuser"})
     fi
-    users=(${users[@]/"shutdown"})
-    users=(${users[@]/"reboot"})
-    users=(${users[@]/"root"})
-    users=(${users[@]/"_mbsetupuser"})
 }
 
 # Função para contar o numero de sessoes para os utilizadores
@@ -53,6 +58,18 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     soId=1 # under GNU/Linux platform
 fi
+
+# processa argumentos
+args=("$@")
+group=""
+for ((a=0; a<$#; a++)); do
+    case ${args[a]} in
+        "-g")
+            group=${args[a+1]}
+            ;;
+    esac
+done
+echo "$group"
 countUsers
 detailSessions
 
