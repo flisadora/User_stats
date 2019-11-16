28e28e6fdc2d1a00fe31999f51cde742cf0ffba6
Isadora <flisadora@gmail.com>
2019-11-16 16:55:21 +0000

    initialize of function countUsers

--- userstats.sh
+++ userstats.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# Função para contar o numero de utilizadores
+function countUsers(){
+    users=$( last | awk '{print $1'} | head -n -2 | sort u )
+    for i in "${users[@]}"; do
+        echo "$i" 
+    done
+}
+
+countUsers
+
+# Função para filtrar e visualizar o número de sessões de um utilizador
+#function numberOfSessions(user){
+#    last | awk
+#}
