d06b13d24bd13b51033cdce33a33e7e9fbcc154c
Gonçalo Matos <gmatos.ferreira@ua.pt>
2019-11-20 17:36:54 +0000

    update to dateConversion of the userstarts.sh

--- userstats.sh
+++ userstats.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
 
 # Função para contar o numero de utilizadores
-function countUsers(){
-    if [ ! -z $group ]; then
-        groupUsers=($( grep "$group:" /etc/group | cut -d ":" -f 4 ))
-        IFS=',' read -r -a users <<< "$groupUsers"
-    else
+function countUsers() {
+    if [ ! -z $group ]; then # Verifica se string $group não é vazia
+        groupUsers=($(grep "$group:" /etc/group | cut -d ":" -f 4)) # Procura no ficheiro /etc/group os utilizadores que pertecem ao grupo
+        IFS=',' read -r -a users <<<"$groupUsers"                   # Faz split da string groupUsers por ',' e cria array na var users
+    else                   # Caso não haja filtragem por grupo, fazer lista dos utilizadores presentes no output do comando last
         if [ $soId -eq 0 ]; then
-            users=($( last | awk '{print $1'} | sed 'N;$!P;$!D;$d' | sort -u ))
+            users=($(last | awk '{print $1'} | sed 'N;$!P;$!D;$d' | sort -u))
         else
-            users=($( last | awk '{print $1'} | head -n -2 | sort -u ))
+            users=($(last | awk '{print $1'} | head -n -2 | sort -u))
         fi
-        users=(${users[@]/"shutdown"})
-        users=(${users[@]/"reboot"})
-        users=(${users[@]/"root"})
-        users=(${users[@]/"_mbsetupuser"})
+        # Remover os utilizadores da lista
+        users=(${users[@]/"shutdown"/})
+        users=(${users[@]/"reboot"/})
+        users=(${users[@]/"root"/})
+        users=(${users[@]/"_mbsetupuser"/})
     fi
+    # Caso haja filtragem por grupo, remover utilizadores que não pertencem ao grupo da lista
     if [ ! -z $regex ]; then
         for i in $users; do
             case "$i" in
-                $regex)
-                    ;;
-                *)
-                    users=(${users[@]/"$i"})
-                    ;;
+            $regex) ;;
+
+            *)
+                users=(${users[@]/"$i"/})
+                ;;
             esac
         done
     fi
 }
 
 # Função para contar o numero de sessoes para os utilizadores
-function detailSessions(){
+function detailSessions() {
     for i in "${users[@]}"; do # para cada utilizador
-        sessions=$(echo -n "$(last | awk '{print $1}' | grep $i | wc -l) ") # conta o numero de sessoes
+        sessions=$(echo -n "$(last $filters | awk '{print $1}' | grep $i | wc -l) ") # conta o numero de sessoes
+        echo $sessions
         if [ $sessions -ne 0 ]; then
             echo -n "$i "
             # seleciona coluna do tempo (array)
             if [ $soId -eq 0 ]; then
                 time=($(last | grep $i | awk '{print $9}'))
-                time=(${time[@]/"in"})
+                time=(${time[@]/"in"/})
             else
                 time=($(last | grep $i | awk '{print $10}'))
-                time=(${time[@]/"no"})
+                time=(${time[@]/"no"/})
             fi
             min=1000000000
             max=0
@@ -49,32 +52,30 @@ function detailSessions(){
             for t in "${time[@]}"; do # percorre tempos de sessao, calcula tempo em minutos e determina tempo minimo e maximo
                 hour=$(echo $t | sed 's/(//' | sed 's/)//' | cut -d ":" -f 1)
                 min=$(echo $t | sed 's/(//' | sed 's/)//' | cut -d ":" -f 2)
-                timeAux=$((10#$hour*60+10#$min)) # 10# para quando operados tem zero a esquerda nao dar erro
-                ((timeSum=$timeSum+$timeAux))
+                timeAux=$((10#$hour * 60 + 10#$min)) # 10# para quando operados tem zero a esquerda nao dar erro
+                ((timeSum = $timeSum + $timeAux))
                 if [ $timeAux -lt $min ]; then
-                    ((min=$timeAux))
+                    ((min = $timeAux))
                 fi
                 if [ $timeAux -gt $max ]; then
-                    ((max=$timeAux))
-                fi            
+                    ((max = $timeAux))
+                fi
             done
             echo "$timeSum $max $min"
         fi
     done
 }
 
-function dateConversion(){
-    echo $dateStr
+function dateConversion() {
     dateStr=$(echo $dateStr | sed 's/"//')
     dateStr=$(date -d "$dateStr" "+%Y-%m-%d %H:%M")
-    echo $dateStr
 }
 
 # main()
 
 # Identifica qual SO esta a correr entre MAC->"Darwin" e Linux->"Linux"
 if [ "$(uname)" == "Darwin" ]; then
-    soId=0 # under Mac OS X platform        
+    soId=0 # under Mac OS X platform
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     soId=1 # under GNU/Linux platform
 fi
@@ -85,28 +86,36 @@ group=""
 regex=""
 dSince=""
 dUntil=""
-for ((a=0; a<$#; a++)); do
+filters=""
+for ((a = 0; a < $#; a++)); do
     case ${args[a]} in
-        "-g")
-            group=${args[a+1]}
-            ;;
-        "-u")
-            regex=${args[a+1]}
-            ;;
-        "-s")
-            dateStr=${args[a+1]}
-            dateConversion
-            dSince=$dateStr
-            echo $dSince
-            ;;
-        "-e")
-            dateStr=${args[a+1]}
-            dateConversion
-            dUntil=$dateStr
-            echo $dUntil
-            ;;                        
+    "-g")
+        group=${args[a + 1]}
+        ;;
+    "-u")
+        regex=${args[a + 1]}
+        ;;
+    "-s")
+        dateStr=${args[a + 1]}
+        dateConversion
+        echo $dateStr
+        echo $filters
+        filters="$filters -s ${dateStr}"
+        echo $filters
+        ;;
+    "-e")
+        dateStr=${args[a + 1]}
+        dateConversion
+        echo $dateStr
+        filters="$filters -t ${dateStr}"
+        echo $filters
+        ;;
     esac
 done
-countUsers
-detailSessions
+len="${#filters}"
+len=$((len-1))
+last "${filters:1:$len}"
+#last -t "$dateStr"
 
+#countUsers
+#detailSessions
