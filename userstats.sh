ec4edfb99494e917112e6993bc8299f93d3cfbaf
Gonçalo Matos <gmatos.ferreira@ua.pt>
2019-11-28 22:32:32 +0000

    made some compatibility changes and arguments validation to userstats

--- userstats.sh
+++ userstats.sh
@@ -17,7 +17,7 @@ function countUsers() {
         users=(${users[@]/"root"/})
         users=(${users[@]/"_mbsetupuser"/})
     fi
-    # Caso haja filtragem por grupo, remover utilizadores que nao pertencem ao grupo da lista
+    # Caso haja filtragem por utilizadores, remover utilizadores que nao pertencem ao grupo da lista
     if [ ! -z $regex ]; then
         for i in $users; do
             case "$i" in
@@ -36,10 +36,10 @@ function detailSessions() {
     nSessions=0
     for i in "${users[@]}"; do # para cada utilizador
         # Selecionar coluna do tempo (array)
-        if [ $soId -eq 0 ]; then
+        if [ $soId -eq 0 ]; then # iOS
             time=($(last | grep $i | awk '{print $9}'))
             time=(${time[@]/"in"/})
-        else
+        else # Linux
             if [ ${#dSince} -ne 0 ] && [ ${#dUntil} -ne 0 ]; then # Filtrar pelo periodo temporal
                 time=($(last -s "$dSince" -t "$dUntil" -f "$file" | grep $i | awk '{print $10}'))
             elif [ ${#dSince} -ne 0 ]; then
@@ -50,6 +50,9 @@ function detailSessions() {
                 time=($(last -f "$file" | grep $i | awk '{print $10}'))
             fi
             time=(${time[@]/"no"/}) # Ignorar sessao atual (sem duracao atribuida)
+            time=(${time[@]/"in"/})
+            time=(${time[@]/"logged"/})
+            time=(${time[@]/"down"/})
         fi
         if [ ${#time[@]} -gt 0 ]; then # Verificar se há registos para esse utilizador
             output+="$i"
@@ -69,7 +72,7 @@ function detailSessions() {
                     ((max = $timeAux))
                 fi
             done
-            output+="\t$nSessions\t$timeSum\t$max\t$min\n" 
+            output+="\t$nSessions\t$timeSum\t$max\t$min\n"
         fi
     done
 }
@@ -88,7 +91,7 @@ function dateConversion() {
 
 # Identificar qual SO esta a correr entre MAC->"Darwin" e Linux->"Linux"
 if [ "$(uname)" == "Darwin" ]; then
-    soId=0 
+    soId=0
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     soId=1
 fi
@@ -125,6 +128,10 @@ for ((a = 0; a < $#; a++)); do
         ;;
     "-f")
         file=${args[a + 1]}
+        if [ ! -f $file ]; then
+            echo "ERROR! The file $file does not exist!"
+            exit
+        fi
         ;;
     "-r")
         sortFilter+=" -r"
@@ -161,6 +168,34 @@ for ((a = 0; a < $#; a++)); do
             exit
         fi
         ;;
+    *) # Validacao das opcoes (se chegou aqui e comeca por "-" e porque e invalida)
+        if [[ ${args[a]} == -* ]]; then
+            echo "ERROR! ${args[a]} is not an option!"
+            echo "The valid options are..."
+            echo -e "\t-g <group>"
+            echo -e "\t\tFilter by group"
+            echo -e "\t-u <user>"
+            echo -e "\t\tFilter by user"
+            echo -e "\t-s <dateSince>"
+            echo -e "\t\tGet data since a certain date"
+            echo -e "\t\teg. \"Sep 19 10:00\""
+            echo -e "\t-e <dateUntil>"
+            echo -e "\t\tGet data until a certain date"
+            echo -e "\t-f <file>"
+            echo -e "\t\tGet data from a specific file"
+            echo -e "\t-r"
+            echo -e "\t\tSort the output in reverse order"
+            echo -e "\t-n"
+            echo -e "\t\tSort the output by number of sessions"
+            echo -e "\t-t"
+            echo -e "\t\tSort the output by total time"
+            echo -e "\t-a"
+            echo -e "\t\tSort the output by maximum time"
+            echo -e "\t-i"
+            echo -e "\t\tSort the output by minimum time"
+            exit
+        fi
+        ;;
     esac
 done
 
@@ -169,4 +204,8 @@ countUsers
 detailSessions
 
 # Output
-echo -e "${output::-2}" | sort $sortFilter
+if [ ${#output} -gt 0 ]; then
+    echo -e "${output::-2}" | sort $sortFilter
+else
+    echo "There is no data to show matching the given options."
+fi
